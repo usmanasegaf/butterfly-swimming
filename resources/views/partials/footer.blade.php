f34a3d5936ddf3476ea620b95270ba99e9218afb
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2025-04-20 19:18:07 +0700

    add footer dan navbar menggunakan blade, resources, views, partials

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,37 @@
+<footer class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4 mb-md-0">
+                <h5 class="mb-3">Butterfly Swimming Course</h5>
+                <p>Kursus renang terbaik di TKI Sport Center dengan metode pembelajaran yang efektif dan menyenangkan
+                    untuk semua usia.</p>
+                <div class="d-flex mt-3">
+                    <a href="#" class="text-white me-3"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="text-white me-3"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-white me-3"><i class="fab fa-youtube"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-whatsapp"></i></a>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4 mb-md-0">
+                <h5 class="mb-3">Jam Operasional</h5>
+                <ul class="list-unstyled">
+                    <li>Senin - Jumat: 08.00 - 20.00</li>
+                    <li>Sabtu: 08.00 - 18.00</li>
+                    <li>Minggu: 09.00 - 16.00</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5 class="mb-3">Kontak Kami</h5>
+                <ul class="list-unstyled">
+                    <li><i class="fas fa-map-marker-alt me-2"></i> TKI Sport Center, Jl. Olahraga No. 123, Jakarta</li>
+                    <li><i class="fas fa-phone me-2"></i> (021) 1234-5678</li>
+                    <li><i class="fas fa-envelope me-2"></i> info@butterflyswim.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-4">
+        <div class="text-center">
+            <p class="mb-0">&copy; {{ date('Y') }} Butterfly Swimming Course. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
