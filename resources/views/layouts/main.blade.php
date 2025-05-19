87a9c31b2b704dab24fa52a8d86926462f9fee16
Mohammad Usman Asegaf <usmanasegaf7@gmail.com>
2025-05-19 13:41:48 +0700

    stage 4

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -33,10 +33,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" href="/">Beranda</a>
+                        <a class="nav-link" href="/">Beranda</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/program">Program</a>
+                        <a class="nav-link" href="/#program">Program</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/jadwal">Jadwal</a>
