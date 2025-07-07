8a28411b594bd579116fb690651efadb41c36d30
Mohammad Usman Asegaf <usmanasegaf7@gmail.com>
2025-07-07 16:28:17 +0700

    menyesuaikan timezone ke asia/jakarta

--- resources/views/guru/attendance/show.blade.php
+++ resources/views/guru/attendance/show.blade.php
@@ -19,7 +19,6 @@
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Detail Jadwal</h6>
-             fungicide }
             <div class="card-body">
                 <p><strong>Kursus:</strong> {{ $schedule->swimmingCourse->name }}</p>
                 <p><strong>Lokasi Kursus:</strong> {{ $schedule->location->name }} (Lat: <span
