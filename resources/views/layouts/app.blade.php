66a9e30c3573b0224dc3e5348d6f2defccbe0838
Mohammad Usman Asegaf <usmanasegaf7@gmail.com>
2025-07-09 16:55:30 +0700

    menyesuaikan custom css publik utama agar merubah default sbadmin2 yang abu ke text-dark, tanpa merusak tampilannya, sehingga keterbacaan makin baik

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                 <div class="container-fluid">
 
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">@yield('title')</h1>
+                        <h1 class="h3 mb-0 text-dark">@yield('title')</h1>
                         @yield('page-actions')
                     </div>
 
