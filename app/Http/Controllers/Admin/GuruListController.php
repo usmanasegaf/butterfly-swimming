b38fce5269090efd2dca80c2f631c31ae16e6e4f
Mohammad Usman Asegaf <usmanasegaf7@gmail.com>
2025-07-10 14:07:16 +0700

    perbaikan save murid

--- app/Http/Controllers/Admin/GuruListController.php
+++ app/Http/Controllers/Admin/GuruListController.php
@@ -1,19 +1,18 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User; // Pastikan ini ada di bagian atas file
-use Illuminate\Http\Request;
 
 class GuruListController extends Controller
 {
     public function index()
     {
         $gurus = User::role('guru') // Mengambil user dengan role 'guru'
-                      ->with('murids') // Meload relasi 'murids' (murid yang dipegang guru)
-                      ->withCount('jadwalGuru') // Menghitung jumlah jadwal yang dibuat guru menggunakan relasi 'jadwalGuru()'
-                      ->get();
+            ->where('status', 'active')
+            ->with('murids')          // Meload relasi 'murids' (murid yang dipegang guru)
+            ->withCount('jadwalGuru') // Menghitung jumlah jadwal yang dibuat guru menggunakan relasi 'jadwalGuru()'
+            ->get();
 
         return view('admin.guru_list', compact('gurus'));
     }
