339eee1aa162704ff3061e95a680367b9940bf1b
Mohammad Usman Asegaf <usmanasegaf7@gmail.com>
2025-07-18 09:25:53 +0700

    tambah course

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -22,7 +22,7 @@
         <div class="container">
             <a class="navbar-brand" href="/">
                 <i class="fas fa-swimming-pool me-2"></i>
-                Butterfly Swimming
+                Butterfly Swimming Course
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
